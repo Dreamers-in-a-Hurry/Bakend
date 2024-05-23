7b43ebc201744b0d600c782476b3c63f66457435
DiegoDefSan <defsan35@gmail.com>
2024-05-23 09:02:42 -0500

    feat: added base model

---
+++ Fitshirt.Infrastructure/Models/Common/BaseModel.cs
@@ -0,0 +1,10 @@
+namespace Fitshirt.Infrastructure.Models.Common;
+
+public abstract class BaseModel
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public int CreatedBy { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public int ModifiedBy { get; set; }
+}
