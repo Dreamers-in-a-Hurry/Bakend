ae2697fb6d033d7bd0f724f0d0714a2acf0308a5
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-05-29 14:13:36 -0500

    feat: implemented controller

---
+++ Fitshirt.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,30 @@
+using AutoMapper;
+using Fitshirt.Api.Dtos.Categories;
+using Fitshirt.Api.Dtos.Colors;
+using Fitshirt.Api.Dtos.Posts;
+using Fitshirt.Api.Dtos.PostsSizes;
+using Fitshirt.Api.Dtos.Sizes;
+using Fitshirt.Infrastructure.Models.Common.Entities;
+using Fitshirt.Infrastructure.Models.Posts;
+using Fitshirt.Infrastructure.Models.Posts.Entities;
+
+namespace Fitshirt.Api.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Post, PostResponse>();
+        CreateMap<Post, ShirtVm>();
+        CreateMap<PostRequest, Post>();
+
+        CreateMap<PostSize, PostSizeResponse>();
+        
+        CreateMap<Size, SizeResponse>();
+
+        CreateMap<Category, CategoryResponse>();
+
+        CreateMap<Color, ColorResponse>();
+    }
+    
+}
