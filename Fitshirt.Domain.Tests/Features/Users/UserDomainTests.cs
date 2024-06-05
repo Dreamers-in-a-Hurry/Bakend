15cdd95a3f52cbc5dcd7c1b9cc31307bf987ca91
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-06-05 12:29:49 -0500

    feat: added UserDomainTest

---
+++ Fitshirt.Domain.Tests/Features/Users/UserDomainTests.cs
@@ -0,0 +1,31 @@
+using Fitshirt.Domain.Features.Users;
+using Fitshirt.Infrastructure.Repositories.Users;
+using Moq;
+
+namespace Fitshirt.Domain.Tests.Features.Users;
+
+public class UserDomainTests
+{
+    private readonly Mock<IUserRepository> _userRepositoryMock;
+    private readonly Mock<IServiceRepository> _serviceRepositoryMock;
+    private readonly Mock<IRoleRepository> _roleRepositoryMock;
+    private readonly UserDomain _userDomain;
+
+    public UserDomainTests()
+    {
+        _userRepositoryMock = new Mock<IUserRepository>();
+        _serviceRepositoryMock = new Mock<IServiceRepository>();
+        _roleRepositoryMock = new Mock<IRoleRepository>();
+
+        _userDomain = new UserDomain(
+            _userRepositoryMock.Object,
+            _serviceRepositoryMock.Object,
+            _roleRepositoryMock.Object
+        );
+    }
+    
+    [Fact]
+    
+    public async Task 
+    
+}
